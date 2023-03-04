b64d888c27cb46cb1ffd7b75c7a4786dc3cbb699
richpoteto <richjiang.soft@gmail.com>
2023-03-04 05:53:40 -0800

    Returned back

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"os"
-
-	"github.com/shpota/goxygen/cli"
-	"github.com/shpota/goxygen/codegen"
 )
 
 func main() {
 	args := os.Args[1:]
-	cli.Start(os.Stdout, args, codegen.Generate)
+	// cli.Start(os.Stdout, args, codegen.Generate)
 	// cli.Start(os.Stdout, args, codegen.Generate)
 }
