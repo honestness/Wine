4a53620bbd6c7d469e7534e4fb51c80b454aecc9
richpoteto <richjiang.soft@gmail.com>
2023-03-04 04:08:34 -0800

    Modify Setting

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/shpota/goxygen/cli"
 	"github.com/shpota/goxygen/codegen"
-	"os"
 )
 
 func main() {
 	args := os.Args[1:]
 	cli.Start(os.Stdout, args, codegen.Generate)
+	// cli.Start(os.Stdout, args, codegen.Generate)
 }
